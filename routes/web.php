53129c30fb61c25121c4824a9088dfd8d467ffea
Leonardo Gonçalves Tavares da Conceição <leonardogtc@gmail.com>
2024-12-08 09:51:59 -0300

    003 - Passando valor pela rota e controller e mostrando na view

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,4 @@ Route::get('/about', function () {
     echo "Sobre o sistema";
 });
 
-Route::get('/main', [MainController::class, 'index']);
+Route::get('/main/{value}', [MainController::class, 'index']);
