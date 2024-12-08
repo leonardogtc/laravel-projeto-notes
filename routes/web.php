4846c110e7b5c0c13f3ebc01bc88a730d3122e3f
Leonardo Gonçalves Tavares da Conceição <leonardogtc@gmail.com>
2024-12-08 08:56:49 -0300

    001 - Introdução às routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "Hello world!";
+});
+
+
+Route::get('/about', function() {
+    echo "Sobre o sistema";
 });
