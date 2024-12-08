d4ad1f19931f738c739aa1400a6774ac44e7b597
Leonardo Gonçalves Tavares da Conceição <leonardogtc@gmail.com>
2024-12-08 09:15:09 -0300

    001 - Criada rota apontando para controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "Hello world!";
 });
+
+
+Route::get('/about', function () {
+    echo "Sobre o sistema";
+});
+
+Route::get('/main', [MainController::class, 'index']);
