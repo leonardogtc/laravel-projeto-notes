abad66a7d3de965cede7ff0dd7d96bab429e7899
Leonardo Gonçalves Tavares da Conceição <leonardogtc@gmail.com>
2024-12-10 23:55:36 -0300

    Criação dos Layouts

--- resources/views/page2.blade.php
+++ resources/views/page2.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main_layout')
 @section('content')
-    <h1>Welcome to the main page</h1>
+    <h1>welcome to page2</h1>
     <hr>
     <h3>O valor é: {{ $value }}</h3>
 @endsection
--- resources/views/page3.blade.php
+++ resources/views/page3.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main_layout')
 @section('content')
-    <h1>Welcome to the main page</h1>
+    <h1>welcome to page3</h1>
     <hr>
     <h3>O valor é: {{ $value }}</h3>
 @endsection
